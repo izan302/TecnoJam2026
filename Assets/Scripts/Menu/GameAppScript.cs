824bed21385d650a43afe0acb09d0220f1c2fe1f
Jan Capdevila <65243539+Goan125@users.noreply.github.com>
2026-02-10 15:07:26 +0100

    Added MainMenu

---
+++ Assets/Scripts/Menu/GameAppScript.cs
@@ -0,0 +1,22 @@
+using Unity.VectorGraphics;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameAppScript : MonoBehaviour
+{
+    [SerializeField] private float m_timeToDoubleClick = 1.5f;
+    private float m_clickCounter;
+    private void Update()
+    {
+        m_clickCounter += Time.deltaTime;
+    }
+    public void AppClick()
+    {
+        if (m_clickCounter < m_timeToDoubleClick)
+        {
+            SceneManager.LoadScene("JanScene2");
+        }
+        m_clickCounter = 0;
+    }
+}
