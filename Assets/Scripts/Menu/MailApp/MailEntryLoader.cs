e134fce3d829ae546c18c8ab453caf3a205a42bf
izan302 <izan302.yt@gmail.com>
2026-02-20 00:22:57 +0100

    Postpo en Desktop scene and mail corrections

--- Assets/Scripts/Menu/MailApp/MailEntryLoader.cs
+++ Assets/Scripts/Menu/MailApp/MailEntryLoader.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 public class MailEntryLoader : MonoBehaviour
 {
     public TextAsset m_textJSON;
-    [SerializeField]public MailLists m_mailLists = new MailLists();
+    [SerializeField] public MailLists m_mailLists = new MailLists();
 
     [Header("Mail Loading")]
     public GameObject m_mailEntryPrefab;
     public GameObject m_MailContent;
     public GameObject m_mailViewPortContent;
     private GameObject m_contentBox;
+
     [Header("Notifications")]
     public GameObject m_notificationGameObject;
     public TextMeshProUGUI m_textUGUI;
     private float m_clickCounter = 0;
-    private float m_timeToDoubleClick = 1.5f;  
+    private float m_timeToDoubleClick = 1.5f;
     public int m_loadedMails { get; private set; }
-    [SerializeField]private int m_newMails;
-
+    [SerializeField] private int m_newMails;
 
     [System.Serializable]
     public class MailJsonData
@@ -30,56 +30,68 @@ public class MailEntryLoader : MonoBehaviour
         public string m_senderDate;
         public int m_level;
     }
-    public class MailLists
-    {
-        public MailJsonData[] mail;
-    }
+
+    public class MailLists { public MailJsonData[] mail; }
 
     void Start()
     {
         m_newMails = 0;
         m_mailLists = JsonUtility.FromJson<MailLists>(m_textJSON.text);
-        for (int i = 0; i<m_mailLists.mail.Length; i++)
+        
+        for (int i = 0; i < m_mailLists.mail.Length; i++)
         {
-            if (m_mailLists.mail[i].m_level != GabeNewell.Instance.m_Level)
+            if (m_mailLists.mail[i].m_level == GabeNewell.Instance.m_Level)
             {
-                continue;
+                CreateMailEntry(i);
             }
-            m_contentBox = Instantiate(m_mailEntryPrefab);
-            m_contentBox.transform.SetParent(m_mailViewPortContent.transform);
-            Mail l_mail = m_contentBox.GetComponent<Mail>();
-            l_mail.SetValues(m_mailLists.mail[i].m_senderPFPstring, m_mailLists.mail[i].m_senderArroba, m_mailLists.mail[i].m_emailContent, m_mailLists.mail[i].m_senderName, m_mailLists.mail[i].m_senderDate, m_MailContent);
-            RectTransform l_rectTransform = m_contentBox.GetComponent<RectTransform>();
-            l_rectTransform.anchoredPosition = new Vector3(-92, -50 * m_loadedMails, 0);
-            m_loadedMails++;
-            m_newMails++;
         }
         Notifiy();
     }
 
     public void LoadLevelMail(int level)
     {
-        for (int i = 0; i<m_mailLists.mail.Length; i++)
+        for (int i = 0; i < m_mailLists.mail.Length; i++)
         {
             if (m_mailLists.mail[i].m_level == level)
             {
-                m_contentBox = Instantiate(m_mailEntryPrefab);
-                m_contentBox.transform.SetParent(m_mailViewPortContent.transform);
-                Mail l_mail = m_contentBox.GetComponent<Mail>();
-                l_mail.SetValues(m_mailLists.mail[i].m_senderPFPstring, m_mailLists.mail[i].m_senderArroba, m_mailLists.mail[i].m_emailContent, m_mailLists.mail[i].m_senderName, m_mailLists.mail[i].m_senderDate, m_MailContent);
-                RectTransform l_rectTransform = m_contentBox.GetComponent<RectTransform>();
-                l_rectTransform.anchoredPosition = new Vector3(-92, -50 * m_loadedMails, 0);
-                m_loadedMails++;
-                m_newMails++;
+                CreateMailEntry(i);
             }
         }
         Notifiy();
     }
+
+    private void CreateMailEntry(int index)
+    {
+        m_contentBox = Instantiate(m_mailEntryPrefab);
+        m_contentBox.transform.SetParent(m_mailViewPortContent.transform, false);
+
+        RectTransform rt = m_contentBox.GetComponent<RectTransform>();
+        rt.localScale = Vector3.one;
+        rt.localPosition = new Vector3(rt.localPosition.x, rt.localPosition.y, 0f);
+        rt.localRotation = Quaternion.identity;
+
+        Mail l_mail = m_contentBox.GetComponent<Mail>();
+        l_mail.SetValues(
+            m_mailLists.mail[index].m_senderPFPstring,
+            m_mailLists.mail[index].m_senderArroba,
+            m_mailLists.mail[index].m_emailContent,
+            m_mailLists.mail[index].m_senderName,
+            m_mailLists.mail[index].m_senderDate,
+            m_MailContent
+        );
+
+        rt.anchoredPosition = new Vector2(-92, -50 * m_loadedMails);
+
+        m_loadedMails++;
+        m_newMails++;
+    }
+
     public void Notifiy()
     {
         m_textUGUI.text = m_newMails.ToString();
         m_notificationGameObject.SetActive(true);
     }
+
     public void OpenMail()
     {
         if (m_clickCounter < m_timeToDoubleClick && !GabeNewell.Instance.m_MailsAreRead)
@@ -92,7 +104,6 @@ public class MailEntryLoader : MonoBehaviour
         m_clickCounter = 0;
     }
 
-    // Update is called once per frame
     void Update()
     {
         m_clickCounter += Time.deltaTime;
