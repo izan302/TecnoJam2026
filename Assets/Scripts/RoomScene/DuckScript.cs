2d14cf66864e3afb78b7cca728e9d5f5d6d9f4c3
izan302 <izan302.yt@gmail.com>
2026-02-21 14:08:10 +0100

    Duck animation

--- Assets/Scripts/RoomScene/DuckScript.cs
+++ Assets/Scripts/RoomScene/DuckScript.cs
@@ -1,14 +1,33 @@
+using System.Collections;
 using UnityEngine;
-[RequireComponent (typeof(AudioSource))]
+
+[RequireComponent(typeof(AudioSource))]
 public class DuckScript : MonoBehaviour
 {
     AudioSource m_AudioSource;
+    public RectTransform m_Duck;
+    public float growSize = 0.2f;
+    public float duration = 0.1f;
+
     private void Start()
     {
         m_AudioSource = GetComponent<AudioSource>();
     }
+
     public void OnMouseDown()
     {
+        StopAllCoroutines();
+        StartCoroutine(QuackPulse());
+    }
+
+    IEnumerator QuackPulse()
+    {
         m_AudioSource.Play();
+        
+        m_Duck.localScale += new Vector3(growSize, growSize, 0f);
+        
+        yield return new WaitForSeconds(duration);
+        
+        m_Duck.localScale -= new Vector3(growSize, growSize, 0f);
     }
 }
