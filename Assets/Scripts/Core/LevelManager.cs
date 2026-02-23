26e6dc88fc4ae797f7f96a11afd61c06bc24a94c
izan302 <izan302.yt@gmail.com>
2026-02-23 09:59:40 +0100

    Fixed typos

--- Assets/Scripts/Core/LevelManager.cs
+++ Assets/Scripts/Core/LevelManager.cs
@@ -51,8 +51,8 @@ public class LevelManager : MonoBehaviour
         gridCellVisual.Setup(grid);
         supplementaryGridCellVisual.Setup(supplementaryGrid);
 
-        //int level = GabeNewell.Instance.m_Level;
-        int level = 1;
+        int level = GabeNewell.Instance.m_Level;
+        //int level = 1;
 
         if (levels != null)
         {
