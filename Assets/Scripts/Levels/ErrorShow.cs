62442b4579ffd5d35b96b1ad4fd3bc1b3f5006c6
izan302 <izan302.yt@gmail.com>
2026-02-24 12:01:57 +0100

    Translation to screen display

--- Assets/Scripts/Levels/ErrorShow.cs
+++ Assets/Scripts/Levels/ErrorShow.cs
@@ -2,11 +2,9 @@ using UnityEngine;
 
 public class ErrorShow : MonoBehaviour
 {
-    public string title = string.Empty;
-    public string message = string.Empty;
-
+    public string errorCode = string.Empty;
     public void _ShowError()
     {
-        PieceInfoDisplayer.instance.DisplayError(title, message);
+        PieceInfoDisplayer.instance.DisplayError(errorCode + "title", errorCode + "message");
     }
 }
