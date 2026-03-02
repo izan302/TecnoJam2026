74a93b695980c95d88bf9dfc133a25f2fa472c86
aimarsito <aimarrcode@gmail.com>
2026-03-02 10:22:40 +0100

    adrituyo

--- Assets/Scripts/Effects/Cursor/InteractiveObject.cs
+++ Assets/Scripts/Effects/Cursor/InteractiveObject.cs
@@ -1,15 +1,26 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using FMODUnity;
 
 public class InteractiveObject : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public bool m_IsInteractable { get; set; } = true;
     private Vector2 hotspot = Vector2.zero;
+    private bool m_check = false;
+    public EventReference m_click = new EventReference();
+
+
+
+    public void Start()
+    {
+
+    }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         if (m_IsInteractable)
         {
+            m_check = true;
             CursorManager.Instance.SetInteractorCursor(CursorManager.CursorImage.Clickable, hotspot);
         }else {
             CursorManager.Instance.SetInteractorCursor(CursorManager.CursorImage.UnClickable, hotspot);
@@ -18,6 +29,7 @@ public class InteractiveObject : MonoBehaviour, IPointerEnterHandler, IPointerEx
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        m_check = false;
         CursorManager.Instance.SetInteractorCursor(CursorManager.CursorImage.Normal, Vector2.zero);
     }
 
@@ -25,4 +37,18 @@ public class InteractiveObject : MonoBehaviour, IPointerEnterHandler, IPointerEx
     {
         CursorManager.Instance.SetInteractorCursor(CursorManager.CursorImage.Normal, Vector2.zero);
     }
+
+
+    public void Update()
+    {
+        if (Input.GetMouseButton (0))
+        {
+            if (m_check)
+            {
+                RuntimeManager.PlayOneShot(m_click, transform.position);
+
+            }
+        }
+    }
+
 }
