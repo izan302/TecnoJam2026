d3c13ea49b4138333d04f934437f822e1f6e9548
izan302 <izan302.yt@gmail.com>
2026-03-02 20:16:24 +0100

    Window limits

--- Assets/Scripts/Menu/UiDragHandler.cs
+++ Assets/Scripts/Menu/UiDragHandler.cs
@@ -6,13 +6,13 @@ public class UIDragHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEn
     private RectTransform m_RectTransform;
     private CanvasGroup m_CanvasGroup;
     private Canvas m_Canvas;
-
+    private RectTransform m_ParentRectTransform;
     void Awake()
     {
         m_RectTransform = GetComponent<RectTransform>();
         m_CanvasGroup = GetComponent<CanvasGroup>();
-        
         m_Canvas = GetComponentInParent<Canvas>();
+        m_ParentRectTransform = transform.parent as RectTransform;
 
         if (m_CanvasGroup == null) m_CanvasGroup = gameObject.AddComponent<CanvasGroup>();
     }
@@ -27,6 +27,25 @@ public class UIDragHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEn
     public void OnDrag(PointerEventData eventData)
     {
         m_RectTransform.anchoredPosition += eventData.delta / m_Canvas.scaleFactor;
+        
+        KeepWithinParent();
+    }
+
+    private void KeepWithinParent()
+    {
+        if (m_ParentRectTransform == null) return;
+
+        Vector2 pos = m_RectTransform.anchoredPosition;
+
+        float minX = (m_ParentRectTransform.rect.width - m_RectTransform.rect.width) / -2f;
+        float maxX = (m_ParentRectTransform.rect.width - m_RectTransform.rect.width) / 2f;
+        float minY = (m_ParentRectTransform.rect.height - m_RectTransform.rect.height) / -2f;
+        float maxY = (m_ParentRectTransform.rect.height - m_RectTransform.rect.height) / 2f;
+
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+        m_RectTransform.anchoredPosition = pos;
     }
 
     public void OnEndDrag(PointerEventData eventData)
